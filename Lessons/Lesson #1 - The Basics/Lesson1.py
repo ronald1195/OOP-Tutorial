ccea6cc48f45dcbfe397a8dae7d1a93fe0b6f11e
Ronald Munoz <ronald1195@icloud.com>
2021-11-03 20:18:21 -0600

    fa

--- Lessons/Lesson #1 - The Basics/Lesson1.py
+++ Lessons/Lesson #1 - The Basics/Lesson1.py
@@ -10,16 +10,97 @@ import resources
 # Display header
 resources.displayLessonIntroMessage(1)
 
+tObject = resources.TestBed()
+
 """
     Variables and Data Types
-    In this section we will apply what we learned about variables and datatypes
+    En esta seccion vamos aplicar lo que hemor aprendido acerca de variables y datatypes.
 """
+print("\n* Variables *\n")
+# --------------------------------------------------------------- #
+# 1. Declara una variable tipo integer llamada mi_integer
+# --------------------------------------------------------------- #
+""" Escribe tu codigo despues de este comentario """
+# mi_integer = 100
 
-# Declare an integer called my_integer
-integer = 0
+# print("El valor de mi_integer es = {} y es tipo {}".format(mi_integer, type(mi_integer)))
+
+# --------------------------------------------------------------- #
+# 2. Declara una variable tipo float llamada mi_float 
+# --------------------------------------------------------------- #
+""" Escribe tu codigo despues de este comentario """
+# mi_float = 2.8
+
+# print("El valor de mi_float es = {} y es tipo {}".format(mi_float, type(mi_float)))
+
+# --------------------------------------------------------------- #
+# 3. Declara una variable tipo string llamada mi_string
+# --------------------------------------------------------------- #
+""" Escribe tu codigo despues de este comentario """
+# mi_string = "Hello World"
+
+# print("El valor de mi_string es = {} y es tipo {}".format(mi_string, type(mi_string)))
+
+# --------------------------------------------------------------- #
+# 4. Declara una variable tipo boolean llamada mi_bool
+# --------------------------------------------------------------- #
+""" Escribe tu codigo despues de este comentario """
+# mi_bool = False
+
+# print("El valor de mi_bool es = {} y es tipo {}".format(mi_bool, type(mi_bool)))
+
+# --------------------------------------------------------------- #
+# 5. Declara una variable tipo complex llamada mi_complex
+# --------------------------------------------------------------- #
+""" Escribe tu codigo despues de este comentario """
+# mi_complex = 1j
+
+# print("El valor de mi_complex es = {} y es tipo {}".format(mi_complex, type(mi_complex)))
+
+# --------------------------------------------------------------- #
+# 6. Declara una variable tipo list llamada mi_list
+# --------------------------------------------------------------- #
+""" Escribe tu codigo despues de este comentario """
+# mi_list = [1,2,3,4,5]
+
+# print("El valor de mi_list es = {} y es tipo {}".format(mi_list, type(mi_list)))
+
+# --------------------------------------------------------------- #
+# 7. Declara una variable tipo dict llamada mi_dict
+# --------------------------------------------------------------- #
+""" Escribe tu codigo despues de este comentario """
+# mi_dict = {"llave":"valor"}
+
+# print("El valor de mi_dict es = {} y es tipo {}".format(mi_dict, type(mi_dict)))
 
 
 
-print("Value of integer = {}".format(integer))
+print("\n* Casting *\n")
+"""
+    Casting
+    Elimina los comentarios de los siguientes fragmentos de codigo 
+    y analiza el resultado. Intenta hacer esto un ejemplo a la vez
+"""
+# --------------------------------------------------------------- #
+# 8. Puedes indicar tipo de las siguientes variables?
+# --------------------------------------------------------------- #
 
-# notes
+# x = float(2)
+# print("El valor de x es = {} y es tipo {}".format(x, type(x)))
+
+# y = str(1000000.000000000)
+# print("El valor de y es = {} y es tipo {}".format(y, type(y)))
+
+# # Presta atencion en que esta pasa con el punto decimal
+# z = str(1000000.0009)
+# print("El valor de y es = {} y es tipo {}".format(y, type(y)))
+
+# Que pasa cuando intentamos forzar un valor int a un str? (ERROR vuelve a comentar este fragmento)
+# i = int('A')
+# print("El valor de i es = {} y es tipo {}".format(i, type(i)))
+
+# w = float("4.2")
+# print("El valor de w es = {} y es tipo {}".format(w, type(w)))
+
+
+tObject.printTestBedSummary()
--- resources.py
+++ resources.py
@@ -14,6 +14,20 @@ L9_HEADER = "                        Lesson # 9"
 L10_HEADER = "                        Lesson # 10" 
 UNDEFINED_HEADER = "             This lesson is not yet published"        
 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    OUTPUTGREY = '\033[90m'
+
+
 def displayLessonIntroMessage(num):
 
     ERROR = False
@@ -46,5 +60,52 @@ def displayLessonIntroMessage(num):
 
     print(LINE)
 
-    if ERROR:
+    if ERROR: 
         exit()
+
+
+class TestBed:
+
+    # class variables
+    total = 0
+
+    succeded_tests = 0
+
+    # Constructor
+    def __init__(self) -> None:
+        pass
+
+    def verifySolution(self, student_output, desired_output):
+        # Increment the amount of tests created for the  test
+        self.total += 1
+
+        print(f"{bcolors.OUTPUTGREY}Your output: {student_output}{bcolors.ENDC}")
+        print(f"{bcolors.OUTPUTGREY}Desired output: {desired_output}{bcolors.ENDC}")
+        if student_output == desired_output:
+            self.succeded_tests += 1
+            self.printSuccessMessage()
+        else:
+            self.printFailMessage()
+
+
+
+    def printSuccessMessage(self):
+        print(f"{bcolors.OKGREEN}Testbed PASSED!{bcolors.ENDC}")
+        print(" ")
+
+    def printFailMessage(self):
+        print(f"{bcolors.FAIL}Testbed FAILED.{bcolors.ENDC}")
+        print(" ")
+
+
+    def printTestBedSummary(self):
+        color = bcolors.OKGREEN if self.total == self.succeded_tests else bcolors.FAIL
+        print(f"{color}------------------- TESTBED COMPLETED -------------------{bcolors.ENDC}")
+        print(f"{color}Summary: [{self.succeded_tests}] of [{self.total}] passed{bcolors.ENDC}")
+
+
+# for i in range(0, 16):
+#     for j in range(0, 16):
+#         code = str(i * 16 + j)
+#         print("\033[" + code + "m " + code.ljust(4))
+#     print("\033[0m")
